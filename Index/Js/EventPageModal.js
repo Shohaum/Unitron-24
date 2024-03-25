41544891779712e0dfb640a958f9877c7cdd2c82
Shohaum <87150922+Shohaum@users.noreply.github.com>
2024-03-26 00:02:17 +0530

    Add files via upload

--- Index/Js/EventPageModal.js
+++ Index/Js/EventPageModal.js
@@ -26,16 +26,33 @@ function loadClicks(){
             const eventDesc = document.querySelector('.event-desc');
             eventDesc.textContent = `${eventdescription[0].desc}`;
 
-            const registerLink = document.querySelector('.register-link');
+            const modalBtnnLinks = document.querySelector(".modal-btn-container");
 
-            if(eventdescription[0].id === 'carnival'){
-                registerLink.innerHTML = `Offline Registration
-                <span class="modal-btn-span"></span>`
-                registerLink.href = '';
+            if(eventdescription[0].id === 'carnival' || eventdescription[0].id === 'uno' || eventdescription[0].id === '8ballpool'){
+                modalBtnnLinks.innerHTML = `
+                <button disabled>
+                    Offline Registration
+                    <span class="modal-btn-span"></span>
+                </button>
+
+                <a href="" class="rule-book">
+                    Rules
+                    <span class="modal-btn-span"></span>
+                </a>`
             }
             else{
-                registerLink.innerHTML = `Register
-                <span class="modal-btn-span"></span>`
+                modalBtnnLinks.innerHTML = `
+                <a href="#" class="register-link">
+                    Register
+                    <span class="modal-btn-span"></span>
+                </a>
+
+                <a href="" class="rule-book">
+                    Rules
+                    <span class="modal-btn-span"></span>
+                </a>
+                `
+                const registerLink = document.querySelector('.register-link');
                 registerLink.href = `${eventdescription[0].link}`;
             }
 
